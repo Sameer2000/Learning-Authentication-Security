a4b12cebc1182ee1ccfd2b94ee22a98e6cd61eee
Sameer <sameerofficial895@gmail.com>
2022-09-06 19:19:35 +0530

    Add npm  mongoose-encryption

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
 
 const app = express();
 
@@ -13,10 +15,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = 'thisisasecret';
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
